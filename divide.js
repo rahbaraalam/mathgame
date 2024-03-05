66c51bbc1560811e3a36d4706ff37720cc1d9bbe
rahbaraalam <103810160+rahbaraalam@users.noreply.github.com>
2024-03-05 12:27:03 +0530

    first commit

---
+++ divide.js
@@ -0,0 +1,34 @@
+var randomNumber1 = Math.floor(Math.random()*20+20)
+var randomNumber2 =Math.floor(Math.random()*10)+1
+var box1 = document.getElementById("box1")
+var box2 = document.getElementById("box2")
+box1.innerHTML=randomNumber1
+box2.innerHTML=randomNumber2
+var answer = Math.floor(randomNumber1/randomNumber2)
+var counter = document.getElementById("score")
+let score=0
+function checkAnswerMath(){
+    var userValue = document.getElementById("userInp").value 
+        
+    for(var i=0;i==false;i++){
+            if(userValue==answer){
+                score=score+1
+                counter.innerHTML=`Score :${score}`
+                
+                var randomNumber1 = Math.floor(Math.random()*40+20)
+                var randomNumber2 =Math.floor(Math.random()*10)+1
+                var box1 = document.getElementById("box1")
+                var box2 = document.getElementById("box2")
+                box1.innerHTML=randomNumber1
+                box2.innerHTML=randomNumber2
+                answer=Math.floor(randomNumber1/randomNumber2)
+            }
+            else{
+                counter.innerHTML=`Wrong, score : ${score}`
+            }             
+    }
+}
+
+
+
+
